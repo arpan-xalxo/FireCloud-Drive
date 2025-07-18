f2b17c272b2737748e1c84c841112c6b3bfdda48
Arpan xalxo <arpan22bcs78@iiitkottayam.ac.in>
2025-07-18 20:16:05 +0530

    Updated

--- config/multer.config.js
+++ config/multer.config.js
@@ -1,14 +1,17 @@
 
 const multer = require('multer')
 const firebase = require('./firebase.config')
-const serviceAccount = require('../drive-bdf61-firebase-adminsdk-fbsvc-e40222e999.json')
 const  firebaseStorage = require('multer-firebase-storage')
 
 
 const storage = firebaseStorage({
-    credentials: firebase.credential.cert(serviceAccount),
-    bucketName:'drive-bdf61.firebasestorage.app',
-    unique:true
+    credentials: {
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    },
+    bucketName: 'drive-bdf61.firebasestorage.app',
+    unique: true
 })
 
 const upload =multer({
