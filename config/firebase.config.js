c567f14cd9d1c2fbc108a33312bdf7eff3a13df5
Arpan xalxo <arpan22bcs78@iiitkottayam.ac.in>
2025-07-18 20:03:32 +0530

    Updated

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,10 +1,13 @@
 const Firebase = require('firebase-admin')
-const serviceAccount = require('../drive-bdf61-firebase-adminsdk-fbsvc-e40222e999.json')
 
 
 const firebase = Firebase.initializeApp({
 
-    credential:Firebase.credential.cert(serviceAccount),
+    credential:Firebase.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
     storageBucket:"drive-bdf61.firebasestorage.app"
 
 })
